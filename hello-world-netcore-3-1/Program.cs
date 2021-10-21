194fa36c319bc0679151c339184394e8c458f6a5
Oscar <oscar@MacBook-Air-de-Oscar.local>
2021-10-21 10:36:38 +0200

    Initial commit

---
+++ hello-world-netcore-3-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_world_netcore_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
